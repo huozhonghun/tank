f0311226213d4fbb98f55b3fd92d87a1142c66d3
linweicheng <279180526@qq.com>
2021-06-21 12:01:21 +0800

    feat: 新增键盘监听对象实体类

--- src/com/huozhonghun/tank/Test.java
+++ src/com/huozhonghun/tank/Test.java
@@ -3,7 +3,7 @@ package com.huozhonghun.tank;
 import com.huozhonghun.tank.enity.TankFrame;
 
 /**
- * @author weichenglin
+ * @author weic henglin
  * @create $2021-06-21-上午 10:56:50
  */
 public class Test {
--- src/com/huozhonghun/tank/enity/TankFrame.java
+++ src/com/huozhonghun/tank/enity/TankFrame.java
@@ -5,19 +5,28 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 /**
+ * 坦克框架对象
+ *
  * @author weichenglin
  * @create $2021-06-21-上午 11:14:30
  */
 public class TankFrame extends Frame {
+
+	// 定义横纵坐标
+	private int x,y=10;
+
 	public TankFrame() {
 		// 窗口大小
-		setSize(800, 600);
+		setSize(800, 800);
 		// 窗口大小是否可调整
 		setResizable(true);
 		// 窗口标题
 		setTitle("坦克大战");
 		// 是否显示窗口
 		setVisible(true);
+
+		// 添加键盘监听对象
+		this.addKeyListener(new MyKeyListener());
 
 		// 点击关闭退出窗口
 		addWindowListener(new WindowAdapter() {
@@ -33,7 +42,7 @@ public class TankFrame extends Frame {
 	@Override
 	public void paint(Graphics g) {
 		// 画出物体的位置和大小
-		g.fillRect(100,100,50,50);
+		g.fillRect(x,y,50,50);
 	}
 
 }
